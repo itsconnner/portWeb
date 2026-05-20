84cc35c67c82f7bdc40227e7e348dcac46095a40
YeyangWang <autumnlikegirl@gmail.com>
2026-05-20 13:50:06 -0400

    Add Netlify deploy config
    - Add netlify.toml with @netlify/plugin-nextjs
    - Drop unused picsum/unsplash remote image patterns

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "picsum.photos" },
-      { protocol: "https", hostname: "fastly.picsum.photos" }
-    ]
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
